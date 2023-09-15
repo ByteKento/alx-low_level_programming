e85e9a861f145717a749864b75948483cacd4671
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-15 07:48:21 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *_isupper - checks for uppercase.
+ *@c: character to be tested.
+ *Return: 1 if c iuppercase else 0.
+ */
+
+int _isupper(int c)
+{
+	if(c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
