0bc8506b78f1c1f9c23bae4037b838dffc69a5a6
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-15 22:59:41 -0700

    prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,12 +2,12 @@
 
 /**
  *more_numbers - prints 10 times the numbers from  0 to 14.
- *Return: Empty.
+ *Return: numbers since o to 14
  */
 
 void more_numbers(void)
 {
-	int x, y;
+	int <x, y;
 
 	for (x = 0; x < 10; x++)
 	{
