153ce13ba1784737f292337a94851858f75910cc
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-18 19:43:42 -0700

    rints every other character of a string, starting with the first character, followed by a new line.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ *puts2 - rints every other character of a string
+ *starting with the first 
+ *character, followed by a new line.
+ *@str: character string
+ */
+
+void puts2(char *str)
+{
+	int m = 0;
+	int t = 0;
+	char *y = str;
+	int x;
+
+	while (*y != '\0')
+	{
+		y++;
+		m++;
+	}
+	t = m -1;
+	
+	for (x = 0; x <= t; x++)
+	{
+		if (x % 2 = 0)
+		{
+			_putchar(str[0]);
+		}
+	}
+	_putchar('\n');
+}
