b9a5832d71d15ab2986f6369d024c0539c47b908
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-22 16:15:39 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,4 @@
-#include " main.h"
+#include "main.h"
 
 /**
  *lett - to encode as given string
@@ -6,20 +6,20 @@
  *Return: Empty
  */
 
-char *leet(char *str)
+char *leet(char *n)
 {
 	int i, j;
 
 	char textchar[] = "aAeEoOtTlL";
 	char encodeMatch[] = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
 		for (j = 0; c[j] != '\0'; j++)
 		{
-			if (str[i] == textchar[j])
+			if (n[i] == textchar[j])
 			{
-				str[i] = encodeMatch[j];
+				n[i] = encodeMatch[j];
 			}
 		}
 	}
