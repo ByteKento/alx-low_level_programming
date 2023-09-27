7dd4a0c8f74d6b1d1e9960da8968f536a18fd1dd
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-27 16:17:28 -0700

    0-puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *_puts_recursion - function to print as sting
+ *@s:string argument
+ *Return: 0(success)
+ */
+
+void _puts_recursion(char *s)
+{
+	printf("%s\n", s);
+}
