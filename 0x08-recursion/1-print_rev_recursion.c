5258495d10c4bd32aa85e51784a503423e4b7d4a
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-27 16:40:19 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *_print_rev_recursion - prints a sring in reverse
+ *@s: given string to reverse
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == "\0")
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
