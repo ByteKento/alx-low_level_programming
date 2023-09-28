8e48153e803bb1425576cd093a8bf9c370b86dca
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-27 17:01:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,7 +7,13 @@
  */
 int _strlen_recursion(char *s)
 {
-	int length = strlen(s);
+	int length = 0;
 
-	return length;
+	if (*s)
+	{
+		length++;
+		length += _strlen_recursion(s + 1);
+	}
+
+	return (length);
 }
