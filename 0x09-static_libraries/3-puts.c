25bb89b41e0f0562bc1561626d6922b120ef5fa5
ByteKento <benardkithumbimunyao@gmail.com>
2023-09-30 09:47:48 -0700

    static library libmy.a containing all the functions

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *_puts - return string
+ *@str: provided string
+ * Return: Always 0 (Success)
+ */
+void _puts(char *str);
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
